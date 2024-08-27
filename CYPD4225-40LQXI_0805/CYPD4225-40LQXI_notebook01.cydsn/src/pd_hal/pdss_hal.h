67aacffc3f1058ba1daae4f5fc052482c71257ae
SUNIX\victo <victor>
2024-08-27 16:40:21 +0800

    no message

--- CYPD4225-40LQXI_0805/CYPD4225-40LQXI_notebook01.cydsn/src/pd_hal/pdss_hal.h
+++ CYPD4225-40LQXI_0805/CYPD4225-40LQXI_notebook01.cydsn/src/pd_hal/pdss_hal.h
@@ -503,7 +503,7 @@ typedef enum
  * Data Struct Definition
  ******************************************************************************/
 
-void change_gl_pdss_status(int number);
+void change_gl_pdss_status(void);
 /**
  * @typedef PD_ADC_CB_T
  * @brief PD ADC comparator interrupt callback type. This callback
